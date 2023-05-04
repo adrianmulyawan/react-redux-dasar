c11b17a889d4bce6826fbd49a95d812c2ee965d3
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-04 13:08:14 +0700

    buat halaman utama

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavbarComponent from '../components/NavbarComponent/navbar.component';
+
+const HomePage = () => {
+  return (
+    <>
+      <NavbarComponent />
+      <div className="container mt-3">
+        <h1>Halaman home</h1>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
