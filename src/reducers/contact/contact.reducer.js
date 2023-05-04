eb36bc4bd1b8155a3d77d673dbaf35b72b1006d3
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-04 14:01:20 +0700

    buat reducer untuk contact

---
+++ src/reducers/contact/contact.reducer.js
@@ -0,0 +1,13 @@
+// # Reducer Kontak
+// > Dibuat untuk menyimpan store atau state yang berhubungan dengan data contact.
+
+const initialState = {};
+
+const contactReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  };
+};
+
+export default contactReducer;
