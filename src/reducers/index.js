f8445cbb0a9b394f7ce9679983858898a715f411
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-04 14:07:53 +0700

    combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import contactReducer from "./contact/contact.reducer";
 
-// > Store digunakan untuk menampung state dari aplikasi yang dibuat.
-// > Karena reducer itu banyak jadi kita harus combine kedalam store kita.
-const store = combineReducers({
+// > combine reducernya, karena dalam 1 aplikasi bisa > 1 reducers.
+// > combine menggunakan method 'combineReducer' dari package redux.
+export default combineReducers({
   contactReducer,
 });
-
-export default store;
