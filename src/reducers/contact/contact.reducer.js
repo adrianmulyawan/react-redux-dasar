7db4181417bafa5de4a9cd9c5134bb35a907f9cf
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 01:01:28 +0700

    buat reducer untuk update state data contact (apakah masih loading, apakah datanya sudah diupdate, atau gagal ditampilkan)

--- src/reducers/contact/contact.reducer.js
+++ src/reducers/contact/contact.reducer.js
@@ -1,10 +1,28 @@
 // # Reducer Kontak
 // > Dibuat untuk menyimpan store atau state yang berhubungan dengan data contact.
 
-const initialState = {};
+// > import constant variable
+import { GET_LIST_CONTACT } from "../../actions/contact.action";
+
+const initialState = {
+  // > State Untuk getListContact
+  getListContactsLoading: false,
+  getListContactsFulfilled: false,
+  getListContactsRejected: false,
+};
 
 const contactReducer = (state = initialState, action) => {
   switch (action.type) {
+    // > case untuk getListContact
+    case GET_LIST_CONTACT:
+      console.info(action.payload, '4. Masuk kedalam reducers getListContacts');
+
+      return {
+        ...state,
+        getListContactsLoading: action.payload.loading,
+        getListContactsFulfilled: action.payload.data,
+        getListContactsRejected: action.payload.errorMessage
+      };
     default:
       return state;
   };
