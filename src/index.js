816d848346a962c25b2516dfdc03d0244b7ba8f6
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-04 13:19:02 +0700

    import package redux, react-redux, dan redux-thunk

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
+// > Redux
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
