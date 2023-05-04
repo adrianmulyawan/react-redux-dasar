df1a2c1e5c40abda1adb6e425d4e2c55a1572a32
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-04 13:16:28 +0700

    Tambah routing untuk halaman not found (tidak ditemukan)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home.page';
+import NotFoundPage from './pages/notFound.page';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <HomePage /> } />
+          <Route path='*' element={ <NotFoundPage /> } />
         </Routes>
       </BrowserRouter>
     </>
