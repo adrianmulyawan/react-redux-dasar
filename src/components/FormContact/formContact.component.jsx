2d882450c591411601d926f2c913231b4cab9969
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 13:35:09 +0700

    edit form

--- src/components/FormContact/formContact.component.jsx
+++ src/components/FormContact/formContact.component.jsx
@@ -24,15 +24,15 @@ const FormContactComponent = () => {
         <form onSubmit={ handleInputContact }>
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Fullname</label>
-            <input type="text" className="form-control" id="name" name='name' onChange={ (e) => setName(e.target.value) } value={ name } required />
+            <input type="text" className="form-control" id="name" name='name' placeholder='Your Fullname' onChange={ (e) => setName(e.target.value) } value={ name } required />
           </div>
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="email" name='email' onChange={ (e) => setEmail(e.target.value) } value={ email } required />
+            <input type="email" className="form-control" id="email" name='email' placeholder='Your Email' onChange={ (e) => setEmail(e.target.value) } value={ email } required />
           </div>
           <div className="mb-3">
             <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
-            <input type="text" className="form-control" id="phoneNumber" name='phoneNumber' onChange={ (e) => setPhoneNumber(e.target.value) } value={ phoneNumber } required />
+            <input type="text" className="form-control" id="phoneNumber" name='phoneNumber' placeholder='Your Phone Number' onChange={ (e) => setPhoneNumber(e.target.value) } value={ phoneNumber } required />
           </div>
           <div className="d-grid gap-2">
             <button className="btn btn-primary" type="submit">
