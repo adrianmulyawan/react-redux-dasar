4ec9b3d5b43ec997ce17a9754b9b6c83c3bad515
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 15:39:23 +0700

    tambah redux dev tools

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import reducers from './reducers'
 
 // > Buat storenya
 // => store digunakan sebagai wadah untuk menyimpan state dari sebuah aplikasi
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
