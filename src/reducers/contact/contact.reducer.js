6c2463005e4c8cb4f9e0a53a6faf9c00067e2ed6
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 13:53:39 +0700

    buat reducer untuk tambah data kontak baru

--- src/reducers/contact/contact.reducer.js
+++ src/reducers/contact/contact.reducer.js
@@ -2,13 +2,21 @@
 // > Dibuat untuk menyimpan store atau state yang berhubungan dengan data contact.
 
 // > import constant variable
-import { GET_LIST_CONTACT } from "../../actions/contact.action";
+import { 
+  GET_LIST_CONTACT,
+  INSERT_NEW_CONTACT
+} from "../../actions/contact.action";
 
 const initialState = {
   // > State Untuk getListContact
   getListContactsLoading: false,
   getListContactsFulfilled: false,
   getListContactsRejected: false,
+
+  // > State Untuk insertNewContact
+  insertNewContactLoading: false,
+  insertNewContactFulfilled: false,
+  insertNewContactRejected: false,
 };
 
 const contactReducer = (state = initialState, action) => {
@@ -22,6 +30,15 @@ const contactReducer = (state = initialState, action) => {
         getListContactsLoading: action.payload.loading,
         getListContactsFulfilled: action.payload.data,
         getListContactsRejected: action.payload.errorMessage
+      };
+    case INSERT_NEW_CONTACT:
+      console.info(action.payload, '4. Masuk kedalam reducers insertNewContact');
+
+      return {
+        ...state,
+        insertNewContactLoading: action.payload.loading,
+        insertNewContactFulfilled: action.payload.data,
+        insertNewContactRejected: action.payload.errorMessage
       };
     default:
       return state;
