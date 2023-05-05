c0137d5fc2bd071ca571b3570ff344a9379bc255
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 13:33:04 +0700

    tambah component untuk input data contact

---
+++ src/components/FormContact/formContact.component.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const FormContactComponent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const data = {
+    name,
+    email,
+    phoneNumber
+  };
+
+  const handleInputContact = (event) => {
+    event.preventDefault();
+
+    console.info(data);
+  }
+
+  return (
+    <>
+      <h5 className='my-3'>Add New Contact</h5>
+      <div className="card p-3">
+        <form onSubmit={ handleInputContact }>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Fullname</label>
+            <input type="text" className="form-control" id="name" name='name' onChange={ (e) => setName(e.target.value) } value={ name } required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="email" name='email' onChange={ (e) => setEmail(e.target.value) } value={ email } required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
+            <input type="text" className="form-control" id="phoneNumber" name='phoneNumber' onChange={ (e) => setPhoneNumber(e.target.value) } value={ phoneNumber } required />
+          </div>
+          <div className="d-grid gap-2">
+            <button className="btn btn-primary" type="submit">
+              Add Contact
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default FormContactComponent;
--- src/pages/contact.page.jsx
+++ src/pages/contact.page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavbarComponent from '../components/NavbarComponent/navbar.component';
 import CardContactComponent from '../components/CardContact/cardContact.component';
+import FormContactComponent from '../components/FormContact/formContact.component';
 
 const ContactPage = () => {
   return (
@@ -9,6 +10,7 @@ const ContactPage = () => {
       <div className="container mt-3">
         <h2>Contact Page</h2>
         <hr />
+        <FormContactComponent />
         <CardContactComponent />
       </div>
     </>
