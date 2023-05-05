2b5c82058280b242234c6883e40c5dd543bbefea
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-05 13:53:25 +0700

    buat action untuk simpan data kontak baru

--- src/actions/contact.action.js
+++ src/actions/contact.action.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // > Buat constant variable (akan dioperkan ke reducers contact)
 // => Digunakan didalam case 
 export const GET_LIST_CONTACT = "GET_LIST_CONTACT";
+export const INSERT_NEW_CONTACT = "INSERT_NEW_CONTACT";
 
 // > Aksi untuk dapatkan seluruh data kontak
 export const getListContacts = () => {
@@ -50,6 +51,49 @@ export const getListContacts = () => {
       });
     };
   };
+};
 
+// > Action untuk tambah data kontak (insert new contact)
+export const insertNewContact = (data) => {
+  console.info('2. Masuk kedalam action insertNewContact');
 
+  return async (dispatch) => {
+    // => handle ketika data masih loading
+    dispatch({
+      type: INSERT_NEW_CONTACT,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false
+      },
+    });
+
+    // => handle ketika insert data rejected fulfilled
+    try {
+      const response = await axios.post('http://localhost:3004/contacts', data);
+      console.info(response.data, '3. Data berhasil ditambahkan!');
+
+      dispatch({
+        type: INSERT_NEW_CONTACT,
+        payload: {
+          loading: false,
+          data: response.data,
+          errorMessage: false,
+        },
+      });
+    } 
+    // => hanlde ketika insert data rejected
+    catch (error) {
+      console.info(error.message, '3. Data gagal ditambahkan!');
+
+      dispatch({
+        type: INSERT_NEW_CONTACT,
+        payload: {
+          loading: false,
+          data: false,
+          errorMessage: error.message,
+        },
+      });
+    };
+  };
 };
