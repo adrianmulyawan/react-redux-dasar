fbe995bda875532c0e0ccb9b0df33120cfb0f167
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-06 10:44:55 +0700

    buat route kehalaman student

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/home.page';
 import NotFoundPage from './pages/notFound.page';
 import ContactPage from './pages/contact.page';
+import StudentPage from './pages/student.page';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={ <HomePage /> } />
           <Route path='/contact' element={ <ContactPage /> } />
+          <Route path='/student' element={ <StudentPage /> } />
           <Route path='*' element={ <NotFoundPage /> } />
         </Routes>
       </BrowserRouter>
