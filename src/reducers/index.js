394fea1a907ce052a2fef737d4ee8ef58be71f44
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-06 10:41:25 +0700

    import student reducer ke reducer utama dan combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import contactReducer from "./contact/contact.reducer";
+import studentReducer from "./student/student.reducer";
 
 // > combine reducernya, karena dalam 1 aplikasi bisa > 1 reducers.
 // > combine menggunakan method 'combineReducer' dari package redux.
 export default combineReducers({
   contactReducer,
+  studentReducer
 });
