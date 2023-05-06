80eb82922d4ec1877b0d58767d7ac103e6c8979b
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-06 10:44:44 +0700

    buat halaman untuk menampilkan data student

---
+++ src/pages/student.page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavbarComponent from '../components/NavbarComponent/navbar.component';
+
+const StudentPage = () => {
+  return (
+    <>
+      <NavbarComponent />
+      <div className="container mt-3">
+        <h2>Student Page</h2>
+        <hr />
+      </div>
+    </>
+  );
+}
+
+export default StudentPage;
