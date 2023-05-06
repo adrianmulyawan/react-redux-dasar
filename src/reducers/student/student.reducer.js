81f6e48778b05046059dbaf13c57d075cca546c4
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-06 13:09:29 +0700

    buat reducer untuk handle menampilkan data students

--- src/reducers/student/student.reducer.js
+++ src/reducers/student/student.reducer.js
@@ -1,7 +1,24 @@
-const initialState = {};
+import { GET_ALL_STUDENTS } from "../../actions/student.action";
+
+const initialState = {
+  // > State untuk action getAllStudents
+  // => kondisi awalnya state bernilai false
+  getAllStudentsLoading: false, // kondisi ketika data loading
+  getAllStudentsFulfilled: false, // kondisi ketika data fulfilled
+  getAllStudentsRejected: false, // kondisi ketika data rejected
+};
 
 const studentReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_ALL_STUDENTS:
+      console.info(action.payload, 'Masuk kedalam reducers getAllStudents');
+
+      return {
+        ...state,
+        getAllStudentsLoading: action.payload.loading,
+        getAllStudentsFulfilled: action.payload.data,
+        getAllStudentsRejected: action.payload.errorMessage
+      };
     default:
       return state;
   }
