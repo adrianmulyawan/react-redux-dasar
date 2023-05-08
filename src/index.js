22c9acec0ebfe4755cda6f5444110758dcdf4004
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-08 12:02:32 +0700

    tambah komentar

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ import reducers from './reducers'
 // > Buat storenya
 // => store digunakan sebagai wadah untuk menyimpan state dari sebuah aplikasi
 const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+console.info(store, 'Masuk kedalam store');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
